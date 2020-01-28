467642bc719ca0696d8cce55609eed865e417238
Nicolas Umana <nicolasumanagarcia@gmail.com>
2020-01-28 17:52:42 -0300

    Updates

---
+++ precache-manifest.22460bffeaba8d4ab13d39a2762679c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa0252bd8e82970b3d6c9ed88c6fc11b",
+    "url": "/sampleapp/index.html"
+  },
+  {
+    "revision": "24b48536f45b9b6bdbe6",
+    "url": "/sampleapp/static/css/main.27091d0b.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/sampleapp/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/sampleapp/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "24b48536f45b9b6bdbe6",
+    "url": "/sampleapp/static/js/main.ab98a6d4.chunk.js"
+  },
+  {
+    "revision": "9516d2b3b4085a9615fc",
+    "url": "/sampleapp/static/js/runtime-main.5dcc8c8b.js"
+  }
+]);
